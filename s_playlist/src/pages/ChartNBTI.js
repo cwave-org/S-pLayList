ba95281c05073631b2657ab9a0f1482f9a24c996
summit45 <brandy2145@gmail.com>
2022-10-01 12:31:56 +0900

    [fix] 패키지 구조 및 네이밍 수정

--- s_playlist/src/pages/ChartNBTI.js
+++ s_playlist/src/pages/ChartNBTI.js
@@ -1,6 +1,6 @@
 import React from "react";
+import Button from "../components/Button";
 import MusicBar from './../components/MusicBar';
-import Button from "../component/Button";
 
 const ChartNBTI=()=>{
 
