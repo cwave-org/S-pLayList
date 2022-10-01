08aa2d0e87830df3358865b71673eed15784b876
summit45 <brandy2145@gmail.com>
2022-10-01 18:25:50 +0900

    [feat] 취향 테스트 시작화면

--- s_playlist/src/pages/TasteTest.js
+++ s_playlist/src/pages/TasteTest.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 import MusicBar from './../components/MusicBar';
 
 const TasteTest=()=>{
 
     return(
         <>
-        test화면
-        <MusicBar pagenum={2} />
+        <div className="topContent">
+
+        </div>
+        
+        <div className="mainContent">
+            <div className="mainimg">
+                <img src = "./images/TasteTest.png" width="80%"></img>
+            </div>
+            <div className="mainText">
+                <div>
+                    카드 사용 내역을 기반으로 하는 <br></br>
+                    객관적인 데이터! <br></br>
+                    이지은님도 몰랐던 이지은님의 취향을 맞춰볼까? <br></br>
+                </div>
+            </div>
+        <div>
+            <div className="mainButton">
+                <Button size="sm" children="시작하기" variant="default_fill"></Button>
+            </div>
+        </div>
+        </div>
+
+        <div className="bottomContent">
+            <MusicBar pagenum={2} />
+        </div>
         </>
     )
 }
