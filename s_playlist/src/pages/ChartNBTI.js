23f560a0527e8dd45b17301ccaa8e034c1b287fd
Nayujin <00nyj@sookmyung.ac.kr>
2022-10-01 12:25:55 +0900

    [feat] MusicBar

--- s_playlist/src/pages/ChartNBTI.js
+++ s_playlist/src/pages/ChartNBTI.js
@@ -1,10 +1,12 @@
 import React from "react";
+import MusicBar from './../components/MusicBar';
 
 const ChartNBTI=()=>{
 
     return(
         <p>
         nbti 화면입니다
+        <MusicBar pagenum={3} />
         </p>
     )
 }
